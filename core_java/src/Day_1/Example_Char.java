a456bd9dabe8610ce204d3c0bc6a0e0d46d279ce
mani <manim@ASUS>
2025-09-23 19:47:04 +0530

    core java

---
+++ core_java/src/Day_1/Example_Char.java
@@ -0,0 +1,28 @@
+package Day_1;
+
+public class Example_Char {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'm';
+		System.out.println(ch);
+
+		// assigning number to char
+		char ch1 = 69;
+		System.out.println(ch1);
+
+		// assigning unicode to char
+		char var1 = '\u00A8';
+		System.out.println(var1);
+
+		// Unicode representation
+		char var2 = '\u20AD';
+		System.out.println(var2);
+
+		// ASCI code representation
+		int a = 'B';
+		System.out.println(a);
+
+	}
+
+}
