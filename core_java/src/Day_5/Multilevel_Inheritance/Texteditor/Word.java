53c4efb169e27e63f8af9c2695890b543de9c47f
mani <manim@ASUS>
2025-10-03 12:20:43 +0530

    core java

---
+++ core_java/src/Day_5/Multilevel_Inheritance/Texteditor/Word.java
@@ -0,0 +1,25 @@
+package Day_5.Multilevel_Inheritance.Texteditor;
+
+public class Word extends WordPad {
+	 private boolean spellCheckEnabled;
+
+	    public Word() {
+	        super();
+	        this.spellCheckEnabled = true;
+	    }
+
+	    public void spellCheck() {
+	        if (spellCheckEnabled) {
+	            System.out.println("Running spell check...");
+	            // Simulate spell check logic here
+	        } else {
+	            System.out.println("Spell check is disabled.");
+	        }
+	    }
+
+	    public void enableSpellCheck(boolean enable) {
+	        spellCheckEnabled = enable;
+	    }
+	
+
+}
---
+++ core_java/src/Day_5/Multilevel_Inheritance/Vehicle/Car.java
@@ -0,0 +1,12 @@
+package Day_5.Multilevel_Inheritance.Vehicle;
+
+public class Car {
+	public Car() {
+		System.out.println("Class Car");
+	}
+
+	public void vehicleType() {
+		System.out.println("Vehicle Type: Car");
+	}
+
+}
