f89aa17732b5b19cd73ca11ecadecf638ac316e9
mani <manim@ASUS>
2025-10-27 19:15:31 +0530

    core java

---
+++ core_java/src/com/tnsif/Day_10/ArrayOfObjectsDemo.java
@@ -0,0 +1,25 @@
+package com.tnsif.Day_10;
+
+import com.tnsif.Day_5.Hierarchial_Inheritance.Student;
+
+public class ArrayOfObjectsDemo {
+	public static void main(String[] args) {
+		// declares an Array of integers.
+		Student[] arr;
+		// allocating memory for 5 objects of type Student.
+		arr = new Student[5];
+		// initialize the first elements of the array
+		arr[0] = new Student();
+		// initialize the second elements of the array
+		arr[1] = new Student();
+		// so on...
+		arr[2] = new Student();
+		arr[3] = new Student();
+		arr[4] = new Student();
+		
+		// accessing the elements of the specified array
+		for (int i = 0; i < arr.length; i++)
+			System.out.println("Element at " + i + " : " + arr[i].getRollNo()+ " " + arr[i].getName());
+	}
+
+}
---
+++ core_java/src/com/tnsif/Day_10/Student.java
@@ -0,0 +1,43 @@
+package com.tnsif.Day_10;
+
+public class Student {
+	private int rollNo;
+	private String name;
+
+	public Student(int rollNo, String name) {
+		this.rollNo = rollNo;
+		this.name = name;
+	}
+
+	public int getRollNo() {
+		return rollNo;
+	}
+
+	public void setRollNo(int rollNo) {
+		this.rollNo = rollNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setNoOfSubjects(int sub) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setMarks(int[] intArr) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setPer(float calculatePercentage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
